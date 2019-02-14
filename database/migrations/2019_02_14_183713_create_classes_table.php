a8e395547a3774a50ae90e1af31a613d682c854b
fabruun <fbruu14@hotmail.com>
2019-02-14 20:47:25 +0100

    database

---
+++ database/migrations/2019_02_14_183713_create_classes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('classname');
+            $table->foreign('classname')->references('rekvirantid')->on('rekvirants');
+            $table->string('teacher');
+            $table->smallInteger('numberofstudents')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
---
+++ database/migrations/2019_02_14_184641_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->string('reservationsid');
+            $table->primary('reservationsid');
+            $table->string('rekvirantid');
+            $table->foreign('rekvirantid')->references('rekvirantid')->on('rekvirants');
+            $table->time('tid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
