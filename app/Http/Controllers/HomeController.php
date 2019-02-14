c9970dd9428a53c9b5bd3d19b23b54dff16f8d6b
fabruun <fbruu14@hotmail.com>
2019-02-14 21:21:17 +0100

    frontpage, remove register and login page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -22,7 +24,9 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
+    { if(Auth::check()){
         return view('home');
+    }
+        return view('welcome');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.loggedin')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Profile</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,9 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                        <a href="#">Bookings</a>
+                        <p style="margin-top: 2em;"><b>Navn: </b>Hr. Nielsen</p>
+                        <p><b>Email: </b>nielsen@gmail.com</p>
                 </div>
             </div>
         </div>
