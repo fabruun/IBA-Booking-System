193aea7b771f6564ee2c92fb3ca8b01a47aa4fe6
fabruun <fbruu14@hotmail.com>
2019-02-18 14:23:11 +0100

    rooms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.loggedin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Profile</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Profile</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
                         <a href="#">Bookinger</a>
                         <p style="margin-top: 2em;"><b>Navn: </b></p>
                         <p><b>Email: </b>nielsen@gmail.com</p>
-
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+s
