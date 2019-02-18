56e089a8546239ca5c0f972e244b250f5cea8909
fabruun <fbruu14@hotmail.com>
2019-02-18 11:58:05 +0100

    delete tables

--- database/migrations/2014_02_14_185347_create_users_table.php
+++ database/migrations/2014_02_14_185347_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->binary('password');
             $table->rememberToken();
+            $table->enum('role', ['student', 'teacher', 'class', 'admin']);
             $table->timestamps();
         });
     }
