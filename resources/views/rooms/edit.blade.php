d35275dea229296c1b15b7f3ac285fbec6d8fb2c
fabruun <fbruu14@hotmail.com>
2019-02-24 15:03:15 +0100

    Canvas

--- resources/views/rooms/edit.blade.php
+++ resources/views/rooms/edit.blade.php
@@ -4,7 +4,7 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-6">
                 <div class="card">
 
                     <div class="card-body">
@@ -22,63 +22,88 @@
                             <li><b>Areal:</b> {{ $room->areasizeofroom }} m<sup>2</sup> </li>
                             <li><b>Antal:</b> {{ $room->personlimit }}</li>
                         </div>
-                        <div style="margin-top:1em; margin-bottom: 1em;">
+                        <div style="margin-top:1em;">
                             <a href="/rooms/{{ $room->id }}"><button class="btn btn-primary" style="margin-right:2em;">Book nu</button></a>
-                            <button class="btn btn-primary" id="getUsers" onclick="getUser()">Get Room</button>
-                            <button class="btn btn-primary" id="getUsers" onclick="getUsers()">Get Rooms</button>
-                            <button id="button" class="btn btn-danger">Say Hello</button>
+                            <button class="btn btn-primary" id="getUsers" onclick="makeCanvas()">Make Canvas</button>
                         </div>
-                        <div id="hello"></div>
-                        <canvas id="canvasRoom"></canvas>
+
+
+
 
                     </div>
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <div class="col-12">
+                        <button id="reset" class="btn btn-primary">Reset</button>
+                        <div class="d-flex" style="margin-top:3em;">
+                            <canvas id="canvasRoom" width="1200" height="1200" class="justify-content-end"></canvas>
+                            <canvas id="toolbox"  style="margin-bottom: 3em; border: 2px solid black;" width="200" height="400"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
 @endsection
 <script>
-    function getUser() {
+
+    function makeCanvas() {
+
+        //XHR
         let xhr = new XMLHttpRequest();
         xhr.open('GET', '{!! asset('room.json') !!}');
-        xhr.onload = function () {
-            if(this.status === 200) {
-                let user = JSON.parse(this.responseText);
-                let output = '';
-                output += '<ul style="list-style: none;">' +
-                        '<li>'+user.roomid+'</li>' +
-                        '<li>'+user.roomwidth+'</li>' +
-                        '<li>'+user.roomlength+'</li>' +
-                        '</ul>';
-                document.getElementById('hello').innerHTML = output;
+        xhr.onload = function() {
+            if(this.status == 200) {
+                let room = JSON.parse(this.responseText);
+                let canvas = document.getElementById('canvasRoom');
+                let ctx = canvas.getContext('2d');
+                width = room.roomwidth * 100;
+                length = room.roomlength * 100;
+                ctx.fillStyle = '#696969';
+                ctx.fillRect(0, 0, width, length);
+
+            }
+            else if(this.status === 404) {
+                document.getElementById('content').innerHTML = "Not found";
             }
         }
+        xhr.onerror = function() {
+            document.getElementById('content').innerHTML = "Request Error";
+        }
         xhr.send();
+
     }
 
-    function getUsers() {
+    function createRooms() {
         let xhr = new XMLHttpRequest();
+
         xhr.open('GET', '{!! asset('rooms.json') !!}');
-        xhr.onload = function () {
-            if(this.status === 200) {
-                let users = JSON.parse(this.responseText);
+        xhr.onload = function() {
+            if(this.status === 202) {
+                let rooms = JSON.parse(this.responseText);
                 let output = '';
-                for(let i in users) {
-                    output += '<ul style="list-style: none;">' +
-                        '<li>' + users.roomid[i] + '</li>' +
-                        '<li>' + users.roomwidth[i] + '</li>' +
-                        '<li>' + users.roomlength[i] + '</li>' +
-                        '</ul>';
+                for(let i in rooms) {
+                    roomnumber = rooms.roomid[i];
+                    width = rooms.roomwidth[i];
+                    length = rooms.roomlength[i];
                 }
-                document.getElementById('hello').innerHTML = output;
             }
         }
-        xhr.send();
     }
 
-window.onload = function() {
-        getUser;
-        getUsers;
-}
+    function makeToolbox() {
+        const canvas = document.getElementById('toolbox');
+        const ctx = canvas.getContext('2d');
+    }
+
+    window.onload = function() {
+        makeToolbox();
+    }
 </script>
