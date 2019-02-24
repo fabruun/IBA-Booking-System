ae7638d02bd7336883798c28d388d0d90ffd5c2c
fabruun <fbruu14@hotmail.com>
2019-02-24 22:32:06 +0100

    canvas update

--- resources/views/rooms/edit.blade.php
+++ resources/views/rooms/edit.blade.php
@@ -24,7 +24,7 @@
                         </div>
                         <div style="margin-top:1em;">
                             <a href="/rooms/{{ $room->id }}"><button class="btn btn-primary" style="margin-right:2em;">Book nu</button></a>
-                            <button class="btn btn-primary" id="getUsers" onclick="makeCanvas()">Make Canvas</button>
+                            <button class="btn btn-primary" id="getUsers" onclick="setup()">Make Canvas</button>
                         </div>
 
 
@@ -38,10 +38,11 @@
             <div class="card">
                 <div class="card-body">
                     <div class="col-12">
-                        <button id="reset" class="btn btn-primary">Reset</button>
+                        <button id="reset" class="btn btn-primary" onclick="reset()">Reset</button>
+                        <button id="reset" class="btn btn-primary" onclick="standard()">Standard indstillinger</button>
                         <div class="d-flex" style="margin-top:3em;">
                             <canvas id="canvasRoom" width="1200" height="1200" class="justify-content-end"></canvas>
-                            <canvas id="toolbox"  style="margin-bottom: 3em; border: 2px solid black;" width="200" height="400"></canvas>
+                           <div id="toolbox"style="height: 600px; width: 300px" ></div>
                         </div>
                     </div>
                 </div>
@@ -55,12 +56,11 @@
 <script>
 
     function makeCanvas() {
-
         //XHR
         let xhr = new XMLHttpRequest();
         xhr.open('GET', '{!! asset('room.json') !!}');
         xhr.onload = function() {
-            if(this.status == 200) {
+            if(this.status === 200) {
                 let room = JSON.parse(this.responseText);
                 let canvas = document.getElementById('canvasRoom');
                 let ctx = canvas.getContext('2d');
@@ -68,7 +68,6 @@
                 length = room.roomlength * 100;
                 ctx.fillStyle = '#696969';
                 ctx.fillRect(0, 0, width, length);
-
             }
             else if(this.status === 404) {
                 document.getElementById('content').innerHTML = "Not found";
@@ -81,29 +80,89 @@
 
     }
 
-    function createRooms() {
+    function reset() {
         let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', '{!! asset('rooms.json') !!}');
+        xhr.open('GET', '{!! asset('room.json') !!}');
         xhr.onload = function() {
-            if(this.status === 202) {
-                let rooms = JSON.parse(this.responseText);
-                let output = '';
-                for(let i in rooms) {
-                    roomnumber = rooms.roomid[i];
-                    width = rooms.roomwidth[i];
-                    length = rooms.roomlength[i];
-                }
+            if(this.status === 200) {
+                let room = JSON.parse(this.responseText);
+                let canvas = document.getElementById('canvasRoom');
+                let ctx = canvas.getContext('2d');
+                width = room.roomwidth * 100;
+                length = room.roomlength * 100;
+                ctx.fillStyle = '#696969';
+                ctx.fillRect(0, 0, width, length);
+            }
+            else if(this.status === 404) {
+                document.getElementById('content').innerHTML = "Not found";
             }
         }
+        xhr.onerror = function() {
+            document.getElementById('content').innerHTML = "Request Error";
+        }
+        xhr.send();
     }
 
-    function makeToolbox() {
-        const canvas = document.getElementById('toolbox');
-        const ctx = canvas.getContext('2d');
+    function standard() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', '{!! asset('room.json') !!}');
+        xhr.onload = function() {
+            if(this.status === 200) {
+                let room = JSON.parse(this.responseText);
+                let canvas = document.getElementById('canvasRoom');
+                let ctx = canvas.getContext('2d');
+                width = room.roomwidth * 100;
+                length = room.roomlength * 100;
+                ctx.fillStyle = '#696969';
+                ctx.fillRect(0, 0, width, length);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(100, 100, 100, 100);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(300, 100, 100, 100);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(500, 100, 100, 100);
+                ctx.fillRect(100, 300, 100, 100);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(300, 300, 100, 100);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(500, 300, 100, 100);
+                ctx.fillRect(100, 500, 100, 100);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(300, 500, 100, 100);
+                ctx.fillStyle = 'black';
+                ctx.fillRect(500, 500, 100, 100);
+
+            }
+            else if(this.status === 404) {
+                document.getElementById('content').innerHTML = "Not found";
+            }
+        }
+        xhr.onerror = function() {
+            document.getElementById('content').innerHTML = "Request Error";
+        }
+        xhr.send();
     }
 
-    window.onload = function() {
-        makeToolbox();
+    function toolbox() {
+        // create toolbox canvas object
+        let body = document.getElementById('toolbox');
+        let toolbox = document.createElement('canvas');
+        toolbox.setAttribute('id', 'toolboxCanvas');
+        body.appendChild(toolbox);
+        document.getElementById('toolboxCanvas').style.width = '300';
+        document.getElementById('toolboxCanvas').style.height = '600';
+        let ctx = toolbox.getContext('2d');
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(0, 0, 300, 600);
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect(50, 50, 150, 150);
+    }
+
+
+
+    setup = function() {
+        makeCanvas();
+        toolbox();
     }
 </script>
